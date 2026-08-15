36239f579d2975e461a8c876dd5a572fcfcf9c28
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-08-15 14:55:39 +0500

    add computer choice for rock paper scissors

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 4);
+    if (computerChoice === 0) {
+        return 'rock';
+    }else if (computerChoice === 1) {
+        return 'paper';
+    }else {
+        return 'scissors';
+    }
+}
+getComputerChoice();
